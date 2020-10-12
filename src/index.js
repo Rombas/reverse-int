40892c688d7bf2290d8ac87a15a9da069b86ff0a
RomanBaraban <Romanbash@gmail.com>
2020-10-12 11:17:57 +0300

    feat: write reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    // let res = n.toString();
+    // let resStr = res.split("");
+    // let resStrRev = resStr.reverse();
+    // resStr = resStrRev.join("");
+    // let ress = Number.parseInt(resStr);
+
+    return Number.parseInt(n.toString().split("").reverse().join(""));
 }
